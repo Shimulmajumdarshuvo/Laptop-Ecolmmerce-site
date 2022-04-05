3d0edf599401149f5e41dd850377564bc8f954ed
shimul <shimul@gmail.com>
2022-04-05 23:37:36 +0600

    Question part added

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -3,26 +3,29 @@ import './Blog.css'
 
 const Blog = () => {
     return (
-        <div>
+        <div className='blog-part'>
             <h1>Question Part</h1>
             <div>
                 <h2>What is the purpose of Context API?</h2>
-                <p>The React Context API is a way for a React app to effectively produce
-                    global variables that can be passed around. This is the alternative to
-                    "prop drilling" or moving props from grandparent to child to parent, and
-                    so on. Context is also touted as an easier, lighter approach to state
-                    management using Redux.
+                <p>The React Context API is a way for a React
+                    app to effectively produce global variables
+                    that can be passed around. This is the alternative
+                    to "prop drilling" or moving props from grandparent
+                    to child to parent, and so on. Context is also touted
+                    as an easier, lighter approach to state management
+                    using Redux.
+
                 </p>
             </div>
             <div><h2>What Semantic Tag?</h2>
-                <p>Semantic HTML or semantic markup is HTML that introduces meaning
-                    to the web page rather than just presentation. For example, a
+                <p>Semantic এইচটিএমএল  or semantic markup হল এইচটিএমএল যা কেবল উপস্থাপনার
+                    পরিবর্তে ওয়েব পৃষ্ঠার অর্থের পরিচয় দেয়। উদাহরণস্বরূপ, একটি P ট্যাগ নির্দেশ করে যে আবদ্ধ
+                    একটি অনুচ্ছেদ। text টি  একটি paragraph.
+                    একটি semantic element clearly describes its meaning to
+                    both the browser and the developer.
+                    Examples of semantic HTML tags include:.</p>
+            </div>
 
-                    tag indicates that the enclosed text is a paragraph.</p>
-            </div>
-            <div>
-                <h2>Inline block and inline-block</h2>
-            </div>
         </div>
     );
 };
